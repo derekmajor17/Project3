1e5c4a9dab694cd15ad27213bfa118de18f05a89
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-12-14 23:42:46 -0500

    Update project.s

--- project.s
+++ project.s
@@ -54,4 +54,10 @@ main:
      move $t0, $v0 
      la $t6,bad
      jal validate 
-     #beq $t6,$t7, invalid
+
+     move $t2, $t6
+     li $v0, 12    # loads userInput in $v0
+     syscall
+     move $t0, $v0 
+     la $t6,bad
+     jal validate  
